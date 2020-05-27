d25ef2dc0fd21ba454fcaa852863043ed4977606
Josh Cook <cookiej91@gmail.com>
2020-05-27 19:58:43 +0100

    Add gitignore and slack api

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const date = new Date();
 const month = (date.getMonth() + 1).toString().padStart(2, "0");
 const todaysDate = `${date.getFullYear()}-${month}-${date.getDate()}`;
 
-console.log(todaysDate);
+console.log(reviewers[todaysDate]);
