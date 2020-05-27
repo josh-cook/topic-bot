82928b3ea244da549476a3b7b3dcf19407d18312
Josh Cook <cookiej91@gmail.com>
2020-05-27 21:26:16 +0100

    Add async/await and interpolate usernames to mention user

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const reviewers = require("./reviewers.json");
 const { WebClient } = require("@slack/web-api");
 
-// Initialize with token for app
 const slack = new WebClient(process.env.SLACK_TOKEN);
 
 const date = new Date();
@@ -9,37 +8,44 @@ const day = date.getDate().toString().padStart(2, "0");
 const month = (date.getMonth() + 1).toString().padStart(2, "0");
 const today = `${day}/${month}`;
 
-slack.users.list().then((response) => console.log(response));
+async function convertToMention(name) {
+  const { members } = await slack.users.list();
 
-slack.conversations
-  .info({
+  for (const user of members) {
+    if (name === user.real_name) {
+      return `<@${user.id}>`;
+    }
+  }
+
+  return name;
+}
+
+(async () => {
+  const info = await slack.conversations.info({
     channel: "C014RE71G8H",
-  })
-  .then((response) => {
-    const reviewer = reviewers[today];
-
-    if (!reviewer) {
-      console.log("No reviewer");
-      return;
-    }
-
-    if (response.channel.topic.value === reviewer) {
-      console.log("Topic already set");
-      return;
-    }
-
-    slack.conversations
-      .setTopic({
-        //specific channel Id and topic you want to assign
-        channel: "C014RE71G8H",
-        topic: reviewer,
-      })
-      .then(
-        () => {
-          console.log("Successfully set topic");
-        },
-        (error) => {
-          console.log(`Failed to set topic: ${error}`);
-        }
-      );
   });
+
+  let reviewer = reviewers[today];
+
+  if (!reviewer) {
+    console.log("No reviewer");
+    return;
+  }
+
+  reviewer = await convertToMention(reviewer);
+
+  if (info.channel.topic.value.includes(reviewer)) {
+    console.log("Topic already set");
+    return;
+  }
+
+  reviewer = `Today's reviewer is: ${reviewer} (${Math.random()})`;
+
+  await slack.conversations.setTopic({
+    //specific channel Id and topic you want to assign
+    channel: "C014RE71G8H",
+    topic: reviewer,
+  });
+
+  console.log("Successfully set topic");
+})();
